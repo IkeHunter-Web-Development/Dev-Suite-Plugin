5818a058d1b50e5f88d27d141c0166492adc5e8c
Isaac Hunter <ikehunter5@gmail.com>
2023-11-12 13:27:12 -0500

    initial functions

--- dev-suite.php
+++ dev-suite.php
@@ -23,3 +23,59 @@
  * Text Domain:       dev-suite
  * Domain Path:       /languages
  */
+
+ // If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'DEV_SUITE_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-plugin-name-activator.php
+ */
+function activate_dev_suite() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+	Dev_Suite_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-plugin-name-deactivator.php
+ */
+function deactivate_dev_suite() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+	Dev_Suite_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_dev_suite' );
+register_deactivation_hook( __FILE__, 'deactivate_dev_suite' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-dev-suite.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_dev_suite() {
+
+	$plugin = new Dev_Suite();
+	$plugin->run();
+
+}
+run_dev_suite();
